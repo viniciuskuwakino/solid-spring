93b681d9c18083dec8b93b5b2578113e86ac6301
Vinicius Kuwakino <vikuwakino@gmail.com>
2023-12-20 11:20:42 -0300

    Extraindo regras de negócio do controller AdocaoController

--- padronizacao/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
+++ padronizacao/src/main/java/br/com/alura/adopet/api/controller/AdocaoController.java
@@ -1,8 +1,10 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.service.AdocaoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,81 +22,30 @@ import java.util.List;
 public class AdocaoController {
 
     @Autowired
-    private AdocaoRepository repository;
-
-    @Autowired
-    private JavaMailSender emailSender;
+    private AdocaoService adocaoService;
 
     @PostMapping
     @Transactional
     public ResponseEntity<String> solicitar(@RequestBody @Valid Adocao adocao) {
-        if (adocao.getPet().getAdotado() == true) {
-            return ResponseEntity.badRequest().body("Pet já foi adotado!");
-        } else {
-            List<Adocao> adocoes = repository.findAll();
-            for (Adocao a : adocoes) {
-                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                    return ResponseEntity.badRequest().body("Tutor já possui outra adoção aguardando avaliação!");
-                }
-            }
-            for (Adocao a : adocoes) {
-                if (a.getPet() == adocao.getPet() && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                    return ResponseEntity.badRequest().body("Pet já está aguardando avaliação para ser adotado!");
-                }
-            }
-            for (Adocao a : adocoes) {
-                int contador = 0;
-                if (a.getTutor() == adocao.getTutor() && a.getStatus() == StatusAdocao.APROVADO) {
-                    contador = contador + 1;
-                }
-                if (contador == 5) {
-                    return ResponseEntity.badRequest().body("Tutor chegou ao limite máximo de 5 adoções!");
-                }
-            }
+        try {
+            this.adocaoService.solicitar(adocao);
+            return ResponseEntity.ok("Adoção solicitada com sucesso!");
+        } catch (ValidacaoException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        adocao.setData(LocalDateTime.now());
-        adocao.setStatus(StatusAdocao.AGUARDANDO_AVALIACAO);
-        repository.save(adocao);
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom("adopet@email.com.br");
-        email.setTo(adocao.getPet().getAbrigo().getEmail());
-        email.setSubject("Solicitação de adoção");
-        email.setText("Olá " +adocao.getPet().getAbrigo().getNome() +"!\n\nUma solicitação de adoção foi registrada hoje para o pet: " +adocao.getPet().getNome() +". \nFavor avaliar para aprovação ou reprovação.");
-        emailSender.send(email);
-
-        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/aprovar")
     @Transactional
     public ResponseEntity<String> aprovar(@RequestBody @Valid Adocao adocao) {
-        adocao.setStatus(StatusAdocao.APROVADO);
-        repository.save(adocao);
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom("adopet@email.com.br");
-        email.setTo(adocao.getTutor().getEmail());
-        email.setSubject("Adoção aprovada");
-        email.setText("Parabéns " +adocao.getTutor().getNome() +"!\n\nSua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi aprovada.\nFavor entrar em contato com o abrigo " +adocao.getPet().getAbrigo().getNome() +" para agendar a busca do seu pet.");
-        emailSender.send(email);
-
+        this.adocaoService.aprovar(adocao);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/reprovar")
     @Transactional
     public ResponseEntity<String> reprovar(@RequestBody @Valid Adocao adocao) {
-        adocao.setStatus(StatusAdocao.REPROVADO);
-        repository.save(adocao);
-
-        SimpleMailMessage email = new SimpleMailMessage();
-        email.setFrom("adopet@email.com.br");
-        email.setTo(adocao.getTutor().getEmail());
-        email.setSubject("Adoção reprovada");
-        email.setText("Olá " +adocao.getTutor().getNome() +"!\n\nInfelizmente sua adoção do pet " +adocao.getPet().getNome() +", solicitada em " +adocao.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")) +", foi reprovada pelo abrigo " +adocao.getPet().getAbrigo().getNome() +" com a seguinte justificativa: " +adocao.getJustificativaStatus());
-        emailSender.send(email);
-
+        this.adocaoService.reprovar(adocao);
         return ResponseEntity.ok().build();
     }
 
---
+++ padronizacao/src/main/java/br/com/alura/adopet/api/exception/ValidacaoException.java
@@ -0,0 +1,7 @@
+package br.com.alura.adopet.api.exception;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String s) {
+        super(s);
+    }
+}
